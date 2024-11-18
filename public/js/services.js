c8fc9b2b1684d9e1388342e427cf103fd8782ed0
atomych <atomychtrue@gmail.com>
2024-11-18 19:26:47 +0300

    services animation

--- public/js/services.js
+++ public/js/services.js
@@ -3,6 +3,26 @@ const servicesSECTIONS = document.querySelectorAll(".services__list-inner");
 
 let activeBTN = servicesBTNS[0];
 
+function animationItems(section) {
+  const items = section.querySelectorAll(".item");
+
+  for (let item of items) {
+    item.classList.add("hiden");
+  }
+
+  let index = 0;
+  const maxIndex = items.length;
+
+  const interval = setInterval(() => {
+    if (index > maxIndex) {
+      clearInterval(interval);
+    } else {
+      items[index].classList.remove("hiden");
+      index += 1;
+    }
+  }, 80);
+}
+
 for (let btn of servicesBTNS) {
   btn.addEventListener("click", () => {
     for (let section of servicesSECTIONS) section.classList.remove("show");
@@ -15,6 +35,7 @@ for (let btn of servicesBTNS) {
         "a" + activeBTN.dataset.index == section.dataset.index
       ) {
         section.classList.add("show");
+        animationItems(section);
       }
     }
   });

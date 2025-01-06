723b0e91f6d32cad05ddbddf640be18b67e0f456
atomych <atomychtrue@gmail.com>
2025-01-06 23:46:02 +0300

    fixed persons, before-after, promotions for admin

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const port = process.env.PORT || 3000;
 
 const services = require("./database/services.json");
 const persons = require("./database/persons.json");
+const beforeAfter = require("./database/beforeAfter.json");
+const promotions = require("./database/promotions.json");
 
 const app = express();
 app.use(cors());
@@ -12,7 +14,11 @@ app.use(express.static("public"));
 app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
-  res.render("main");
+  res.render("main", {
+    beforeAfter: beforeAfter.filter((el) => el.persons.includes("main")),
+    team: persons,
+    promo: promotions,
+  });
 });
 
 app.get("/price", (req, res) => {
@@ -20,11 +26,14 @@ app.get("/price", (req, res) => {
 });
 
 app.get("/service", (req, res) => {
-  res.render("service", services.filter(el => el.id == req.query.id)[0]);
+  res.render("service", services.filter((el) => el.id == req.query.id)[0]);
 });
 
 app.get("/person", (req, res) => {
-  res.render("person", persons.filter(el => el.id == req.query.id)[0]);
+  res.render("person", {
+    person: persons.filter((el) => el.id == req.query.id)[0],
+    beforeAfter: beforeAfter.filter((el) => el.persons.includes(req.query.id)),
+  });
 });
 
 app.listen(port);
